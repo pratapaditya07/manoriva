6fafda260d72525ca5a942acb222dcdb5de6e04c
pratapaditya07 <139495535+pratapaditya07@users.noreply.github.com>
2025-01-11 20:17:24 +0530

    added a new page

--- script.js
+++ script.js
@@ -3,9 +3,63 @@ gsap.to("#mainimage",{
     scrollTrigger:{
         trigger: "#mainimage",
         scroller: "body",
-        start:"top 35%",
+        start:"top 30%",
         end:"top -30%",
         scrub: 1
+    }
+})
 
+gsap.from("#ll",{
+    scale:0,
+    scrollTrigger:{
+        trigger: "#ll",
+        scroller: "body",
+        start:"top 90%",
+        end:"top 60%",
+        scrub: 1
+    }
+})
+
+gsap.from("#l",{
+    scale:0,
+    scrollTrigger:{
+        trigger: "#l",
+        scroller: "body",
+        start:"top 90%",
+        end:"top 60%",
+        scrub: 1
+    }
+})
+
+gsap.from("#m",{
+    scale:0,
+    scrollTrigger:{
+        trigger: "#m",
+        scroller: "body",
+        start:"top 110%",
+        end:"top 60%",
+        scrub: 1
+    }
+})
+
+gsap.from("#r",{
+    scale:0,
+    scrollTrigger:{
+        trigger: "#r",
+        scroller: "body",   
+        start:"top 90%",
+        end:"top 60%",
+        scrub: 1
+    }
+})
+
+gsap.from("#rr",{
+    scale:0,
+    scrollTrigger:{
+        trigger: "#rr",
+        scroller: "body",
+        start:"top 90%",
+        end:"top 60%",
+        scrub: 1
     }
 })
