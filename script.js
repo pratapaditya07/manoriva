0110fc942b094591589a7b993be9380285712a1d
pratapaditya07 <139495535+pratapaditya07@users.noreply.github.com>
2025-07-14 02:18:53 +0530

    added some pages
    need to do a lot of work

--- script.js
+++ script.js
@@ -9,27 +9,7 @@ gsap.to("#mainimage",{
     }
 })
 
-/* gsap.from("#ll",{
-    scale:0,
-    scrollTrigger:{
-        trigger: "#ll",
-        scroller: "body",
-        start:"top 90%",
-        end:"top 60%",
-        scrub: 1
-    }
-})
 
-gsap.from("#l",{
-    scale:0,
-    scrollTrigger:{
-        trigger: "#l",
-        scroller: "body",
-        start:"top 90%",
-        end:"top 60%",
-        scrub: 1
-    }
-}) */
 
 gsap.from("#m",{
     scale:0,
@@ -41,25 +21,3 @@ gsap.from("#m",{
         scrub: 1
     }
 })
-
-/* gsap.from("#r",{
-    scale:0,
-    scrollTrigger:{
-        trigger: "#r",
-        scroller: "body",   
-        start:"top 90%",
-        end:"top 60%",
-        scrub: 1
-    }
-})
-
-gsap.from("#rr",{
-    scale:0,
-    scrollTrigger:{
-        trigger: "#rr",
-        scroller: "body",
-        start:"top 90%",
-        end:"top 60%",
-        scrub: 1
-    }
-}) */
